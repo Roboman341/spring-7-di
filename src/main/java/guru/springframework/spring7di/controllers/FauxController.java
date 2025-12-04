eae1f9783f9f2443d0970cdca1570fe7bdc24ba1
Robert Venhryn <robert.venhryn@innocode.no>
2025-12-04 10:07:38 +0200

    fix

---
+++ src/main/java/guru/springframework/spring7di/controllers/FauxController.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring7di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FauxController {
+
+
+}
---
+++ src/main/java/guru/springframework/spring7di/services/GetDatasourceSettingsService.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring7di.services;
+
+public interface GetDatasourceSettingsService {
+
+    String getDatasourceSettings (String datasource);
+}
